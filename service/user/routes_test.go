b0548dccd97c21bf73baa1ed762fbe5a6c8b4997
AyKrimino <ayoub.krimi14@gmail.com>
2025-03-28 22:47:34 +0100

    test: add tests for login

--- service/user/routes_test.go
+++ service/user/routes_test.go
@@ -2,6 +2,7 @@ package user
 
 import (
 	"database/sql"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -233,6 +234,159 @@ func TestHandleRegister(t *testing.T) {
 		_, err := handler.UserRepo.GetUserByEmail(testEmail)
 		if err == nil {
 			t.Fatal("expected user fetching to fail.")
+		}
+	})
+}
+
+func TestHandleLogin(t *testing.T) {
+	testEmail := "test@example.com"
+
+	t.Run("Valid credentials", func(t *testing.T) {
+		handler, db := SetupHandlerWithDB(t)
+		defer db.Close()
+
+		registerReqBody := `{
+		"email": "` + testEmail + `",
+		"password": "password123",
+		"role": "JobSeeker",
+		"firstName": "John",
+		"lastName": "Doe",
+		"profileSummary": "Software Engineer",
+		"skills": ["Go", "Python"],
+		"experience": 2,
+		"education": "BSc Computer Science"
+		}`
+
+		req := httptest.NewRequest("POST", "/register", strings.NewReader(registerReqBody))
+		req.Header.Set("Content-Type", "application/json")
+		rec := httptest.NewRecorder()
+
+		handler.handleRegister(rec, req)
+
+		res := rec.Result()
+		defer res.Body.Close()
+
+		if res.StatusCode != http.StatusCreated {
+			t.Fatalf("expected status code 201, got %d", res.StatusCode)
+		}
+
+		createdUser, err := handler.UserRepo.GetUserByEmail(testEmail)
+		if err != nil {
+			t.Fatalf("failed to fetch user from DB: %v", err)
+		}
+
+		if createdUser.Email != testEmail {
+			t.Fatalf("expected user email to be `%s`, got `%s`", testEmail, createdUser.Email)
+		}
+
+		if createdUser.Role != "JobSeeker" {
+			t.Fatalf("expected user role to be `JobSeeker`, got `%s`", createdUser.Role)
+		}
+
+		loginReqBody := `{
+		"email": "` + testEmail + `",
+		"password": "password123"
+		}`
+
+		req2 := httptest.NewRequest("POST", "/login", strings.NewReader(loginReqBody))
+		req2.Header.Set("Content-Type", "application/json")
+		rec2 := httptest.NewRecorder()
+
+		handler.handleLogin(rec2, req2)
+
+		res2 := rec2.Result()
+		defer res2.Body.Close()
+
+		if res2.StatusCode != http.StatusOK {
+			t.Fatalf("expected status code 200, got %d", res2.StatusCode)
+		}
+
+		body, _ := io.ReadAll(res2.Body)
+		if !strings.Contains(string(body), "token") {
+			t.Fatal("expected token to be returned")
+		}
+	})
+
+	t.Run("Invalid request body missing required password field", func(t *testing.T) {
+		handler, db := SetupHandlerWithDB(t)
+		defer db.Close()
+
+		loginReqBody := `{
+		"email": "` + testEmail + `"
+		}`
+
+		req := httptest.NewRequest("POST", "/login", strings.NewReader(loginReqBody))
+		req.Header.Set("Content-Type", "application/json")
+		rec := httptest.NewRecorder()
+
+		handler.handleLogin(rec, req)
+
+		res := rec.Result()
+		defer res.Body.Close()
+
+		if res.StatusCode != http.StatusBadRequest {
+			t.Fatalf("expected status code 400, got %d", res.StatusCode)
+		}
+	})
+
+	t.Run("Invalid password", func(t *testing.T) {
+		handler, db := SetupHandlerWithDB(t)
+		defer db.Close()
+
+		registerReqBody := `{
+		"email": "` + testEmail + `",
+		"password": "password123",
+		"role": "JobSeeker",
+		"firstName": "John",
+		"lastName": "Doe",
+		"profileSummary": "Software Engineer",
+		"skills": ["Go", "Python"],
+		"experience": 2,
+		"education": "BSc Computer Science"
+		}`
+
+		req := httptest.NewRequest("POST", "/register", strings.NewReader(registerReqBody))
+		req.Header.Set("Content-Type", "application/json")
+		rec := httptest.NewRecorder()
+
+		handler.handleRegister(rec, req)
+
+		res := rec.Result()
+		defer res.Body.Close()
+
+		if res.StatusCode != http.StatusCreated {
+			t.Fatalf("expected status code 201, got %d", res.StatusCode)
+		}
+
+		createdUser, err := handler.UserRepo.GetUserByEmail(testEmail)
+		if err != nil {
+			t.Fatalf("failed to fetch user from DB: %v", err)
+		}
+
+		if createdUser.Email != testEmail {
+			t.Fatalf("expected user email to be `%s`, got `%s`", testEmail, createdUser.Email)
+		}
+
+		if createdUser.Role != "JobSeeker" {
+			t.Fatalf("expected user role to be `JobSeeker`, got `%s`", createdUser.Role)
+		}
+
+		loginReqBody := `{
+		"email": "` + testEmail + `",
+		"password": "invalidPassword123"
+		}`
+
+		req2 := httptest.NewRequest("POST", "/login", strings.NewReader(loginReqBody))
+		req2.Header.Set("Content-Type", "application/json")
+		rec2 := httptest.NewRecorder()
+
+		handler.handleLogin(rec2, req2)
+
+		res2 := rec2.Result()
+		defer res2.Body.Close()
+
+		if res2.StatusCode != http.StatusBadRequest {
+			t.Fatalf("expected status code 400, got %d", res2.StatusCode)
 		}
 	})
 }
